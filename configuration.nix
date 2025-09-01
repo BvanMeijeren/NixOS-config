8874eb28b9859bb2ed9da19f4c651c246d5a9155
BvanMeijeren <bastiaan-vanmeijeren@hotmail.com>
2025-09-01 22:29:43 +0200

    added new DE choices and made switching/trying easier

--- configuration.nix
+++ configuration.nix
@@ -64,53 +64,7 @@ programs.ssh = {
 
 services.openssh.enable = true;
 
-### DESKTOP ENVIRONMENT ###
-# Enable the X11 windowing system.
-services.xserver.enable = true;
-
-programs.sway = {
-  enable = true;
-  wrapperFeatures.gtk = true; # So GTK apps work properly
-};
-
-# Allow running sway without root privileges
-security.pam.services.swaylock = {};
-
-# Boot directly into sway
-# services.greetd = {
-#   enable = true;
-#   settings.default_session.command = "sway";
-# };
-
-# ###########
-# ## GNOME ##
-###########
-# Enable the GNOME Desktop Environment.
-# services.xserver.displayManager.gdm.enable = true;
-# services.xserver.desktopManager.gnome.enable = true;
-
-# # Unwanted packages that come with Gnome need to be excluded
-# environment.gnome.excludePackages = (
-#   with pkgs;
-#   [
-#     atomix # puzzle game
-#     cheese # webcam tool
-#     epiphany # web browser
-#     evince # document viewer
-#     geary
-#     # email reader
-#     gedit # text editor
-#     gnome-characters
-#     gnome-music
-#     gnome-photos
-#     gnome-terminal
-#     gnome-tour
-#     hitori # sudoku game
-#     iagno # go game
-#     tali # poker game
-#     totem # video player
-#   ]
-# );
+programs.hyprland.enable = true; # enable Hyprland
 
 ### THEME ###
 # Font
@@ -183,16 +137,15 @@ environment.systemPackages = with pkgs; [
   waybar
   wofi               # application launcher
   alacritty          # terminal emulator
+  kitty
   foot
-  grim slurp         # screenshot tools
-  wl-clipboard       # clipboard tools
-  mako               # notifications
-  brightnessctl      # brightness control
-  swaybg             # sway backgrounds
-  pipewire           # audio control (or pipewire-pulse if using PipeWire)
-  pulseaudio
   openssh
-  #ghostty
+  ## DE specific for sway ##
+  #grim slurp         # screenshot tools
+  #wl-clipboard       # clipboard tools
+  #mako               # notifications
+  #brightnessctl      # brightness control
+  #swaybg             # sway backgrounds
   ## Software development-related ##
   git
   vim
@@ -209,17 +162,19 @@ environment.systemPackages = with pkgs; [
   ripgrep
   luarocks # lua module support (neovim dependecy)
   ## Audio production ##
+  pipewire           # audio control (or pipewire-pulse if using PipeWire)
+  pulseaudio
   reaper
   ## Entertainment ##
   spotify
   #steam
 ];
 
-# Install firefox.
-programs.firefox.enable = true;
-
 # Allow unfree packages
 nixpkgs.config.allowUnfree = true;
+
+# Enable firefox.
+programs.firefox.enable = true;
 
 # Enable Docker
 virtualisation.docker.enable = true;

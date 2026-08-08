cf75e9bebec8b6d628fc153f23952d0191ac7436
BvanMeijeren <bastiaan-vanmeijeren@hotmail.com>
2026-08-08 14:58:43 +0200

    expanded on the system update script to also clean up old builds

--- update_system.sh
+++ update_system.sh
@@ -1,3 +1,7 @@
 cd /home/bastiaan/Documents/git/
 # Run the following command from the Documents/git directory to update system
 sudo nixos-rebuild switch --flake /home/bastiaan/Documents/git/NixOS-config#nixos
+
+# clean up old builds, keep last 3
+sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations +3
+sudo nix-collect-garbage
